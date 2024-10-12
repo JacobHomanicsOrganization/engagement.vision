b660b3250498ec5ea366e8f3c0c24fb2a7db49e1
Jacob Homanics <homanicsjake@gmail.com>
2024-10-12 10:10:21 -0500

    abstracted filtered components

--- packages/nextjs/utils/how-based-are-you/filterFarcasterMessagesForTally.ts
+++ packages/nextjs/utils/how-based-are-you/filterFarcasterMessagesForTally.ts
@@ -1,4 +1,4 @@
-import { getTallyForFilteredArray } from "./getTallyForFilteredArray";
+// import { getTallyForFilteredArray } from "./getTallyForFilteredArray";
 import { Chain } from "viem";
 
 const FARCASTER_START_EPOCH = 1609459200;
@@ -12,8 +12,8 @@ const chainsObjs = {
   },
 };
 
-export function getAllTimeFarcasterMessagesTally(array: any[], pointsPer: number, chain: Chain) {
-  return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+export function getAllTimeFarcasterMessages(array: any[], chain: Chain) {
+  return array.filter(element => {
     let isPresent = false;
 
     for (let i = 0; i < element.data.castAddBody?.mentions.length; i++) {
@@ -29,8 +29,8 @@ export function getAllTimeFarcasterMessagesTally(array: any[], pointsPer: number
   });
 }
 
-export function getYearlyFarcasterMessagesTally(array: any[], pointsPer: number, chain: Chain, year: number) {
-  return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+export function getYearlyFarcasterMessages(array: any[], year: number, chain: Chain) {
+  return array.filter(element => {
     const date = new Date((FARCASTER_START_EPOCH + element.data.timestamp) * 1000);
     let isPresent = false;
     for (let i = 0; i < element.data.castAddBody?.mentions.length; i++) {
@@ -46,14 +46,8 @@ export function getYearlyFarcasterMessagesTally(array: any[], pointsPer: number,
   });
 }
 
-export function getMonthlyFarcasterMessagesTally(
-  array: any[],
-  pointsPer: number,
-  chain: Chain,
-  year: number,
-  month: number,
-) {
-  return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+export function getMonthlyFarcasterMessages(array: any[], year: number, month: number, chain: Chain) {
+  return array.filter(element => {
     const date = new Date((FARCASTER_START_EPOCH + element.data.timestamp) * 1000);
     let isPresent = false;
     for (let i = 0; i < element.data.castAddBody?.mentions.length; i++) {
@@ -69,15 +63,8 @@ export function getMonthlyFarcasterMessagesTally(
   });
 }
 
-export function getDailyFarcasterMessagesTally(
-  array: any[],
-  pointsPer: number,
-  chain: Chain,
-  year: number,
-  month: number,
-  day: number,
-) {
-  return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+export function getDailyFarcasterMessage(array: any[], year: number, month: number, day: number, chain: Chain) {
+  return array.filter(element => {
     const date = new Date((FARCASTER_START_EPOCH + element.data.timestamp) * 1000);
     let isPresent = false;
     for (let i = 0; i < element.data.castAddBody?.mentions.length; i++) {
@@ -91,4 +78,92 @@ export function getDailyFarcasterMessagesTally(
     }
     return isPresent && date.getFullYear() === year && date.getMonth() + 1 === month && date.getDate() === day;
   });
+}
+
+export function getAllTimeFarcasterMessagesTally(array: any[], pointsPer: number, chain: Chain) {
+  return getAllTimeFarcasterMessages(array, chain).length * pointsPer;
+  // return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+  //   let isPresent = false;
+
+  //   for (let i = 0; i < element.data.castAddBody?.mentions.length; i++) {
+  //     for (let j = 0; j < chainsObjs[chain?.name as keyof typeof chainsObjs]?.mentionFids.length; j++) {
+  //       if (
+  //         element.data.castAddBody.mentions[i] === chainsObjs[chain?.name as keyof typeof chainsObjs]?.mentionFids[j]
+  //       ) {
+  //         isPresent = true;
+  //       }
+  //     }
+  //   }
+  //   return isPresent;
+  // });
+}
+
+export function getYearlyFarcasterMessagesTally(array: any[], pointsPer: number, chain: Chain, year: number) {
+  return getYearlyFarcasterMessages(array, year, chain).length * pointsPer;
+
+  // return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+  //   const date = new Date((FARCASTER_START_EPOCH + element.data.timestamp) * 1000);
+  //   let isPresent = false;
+  //   for (let i = 0; i < element.data.castAddBody?.mentions.length; i++) {
+  //     for (let j = 0; j < chainsObjs[chain?.name as keyof typeof chainsObjs]?.mentionFids.length; j++) {
+  //       if (
+  //         element.data.castAddBody.mentions[i] === chainsObjs[chain?.name as keyof typeof chainsObjs]?.mentionFids[j]
+  //       ) {
+  //         isPresent = true;
+  //       }
+  //     }
+  //   }
+  //   return isPresent && date.getFullYear() === year;
+  // });
+}
+
+export function getMonthlyFarcasterMessagesTally(
+  array: any[],
+  pointsPer: number,
+  chain: Chain,
+  year: number,
+  month: number,
+) {
+  return getMonthlyFarcasterMessages(array, year, month, chain).length * pointsPer;
+
+  // return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+  //   const date = new Date((FARCASTER_START_EPOCH + element.data.timestamp) * 1000);
+  //   let isPresent = false;
+  //   for (let i = 0; i < element.data.castAddBody?.mentions.length; i++) {
+  //     for (let j = 0; j < chainsObjs[chain?.name as keyof typeof chainsObjs]?.mentionFids.length; j++) {
+  //       if (
+  //         element.data.castAddBody.mentions[i] === chainsObjs[chain?.name as keyof typeof chainsObjs]?.mentionFids[j]
+  //       ) {
+  //         isPresent = true;
+  //       }
+  //     }
+  //   }
+  //   return isPresent && date.getFullYear() === year && date.getMonth() + 1 === month;
+  // });
+}
+
+export function getDailyFarcasterMessagesTally(
+  array: any[],
+  pointsPer: number,
+  chain: Chain,
+  year: number,
+  month: number,
+  day: number,
+) {
+  return getDailyFarcasterMessage(array, year, month, day, chain).length * pointsPer;
+
+  // return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+  //   const date = new Date((FARCASTER_START_EPOCH + element.data.timestamp) * 1000);
+  //   let isPresent = false;
+  //   for (let i = 0; i < element.data.castAddBody?.mentions.length; i++) {
+  //     for (let j = 0; j < chainsObjs[chain?.name as keyof typeof chainsObjs]?.mentionFids.length; j++) {
+  //       if (
+  //         element.data.castAddBody.mentions[i] === chainsObjs[chain?.name as keyof typeof chainsObjs]?.mentionFids[j]
+  //       ) {
+  //         isPresent = true;
+  //       }
+  //     }
+  //   }
+  //   return isPresent && date.getFullYear() === year && date.getMonth() + 1 === month && date.getDate() === day;
+  // });
 }
--- packages/nextjs/utils/how-based-are-you/filterOnchainTransactionsForTally.ts
+++ packages/nextjs/utils/how-based-are-you/filterOnchainTransactionsForTally.ts
@@ -1,21 +1,50 @@
-import { getTallyForFilteredArray } from "./getTallyForFilteredArray";
+// import { getTallyForFilteredArray } from "./getTallyForFilteredArray";
 
-export function getAllTimeOnchainTransactionsTally(array: any[], pointsPer: number) {
-  return getTallyForFilteredArray(array, pointsPer);
-}
-
-export function getYearlyOnchainTransactionsTally(array: any[], pointsPer: number, year: number) {
-  return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+export function getYearlyOnchainTransactions(array: any[], year: number) {
+  return array.filter(element => {
     const date = new Date(element.timeStamp * 1000);
     return date.getFullYear() === year;
   });
 }
 
-export function getMonthlyOnchainTransactionsTally(array: any[], pointsPer: number, year: number, month: number) {
-  return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+export function getMonthlyOnchainTransactions(array: any[], year: number, month: number) {
+  return array.filter(element => {
     const date = new Date(element.timeStamp * 1000);
     return date.getFullYear() === year && date.getMonth() + 1 === month;
   });
+}
+
+export function getDailyOnchainTransactions(array: any[], year: number, month: number, day: number) {
+  return array.filter(element => {
+    const date = new Date(element.timeStamp * 1000);
+
+    const isWithinYear = date.getFullYear() === year;
+    const isWithinMonth = date.getMonth() + 1 === month;
+    const isWithinDay = date.getDate() === day;
+    return isWithinYear && isWithinMonth && isWithinDay;
+  });
+}
+
+export function getAllTimeOnchainTransactionsTally(array: any[], pointsPer: number) {
+  return array.length * pointsPer;
+}
+
+export function getYearlyOnchainTransactionsTally(array: any[], pointsPer: number, year: number) {
+  return getYearlyOnchainTransactions(array, year).length * pointsPer;
+
+  // return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+  //   const date = new Date(element.timeStamp * 1000);
+  //   return date.getFullYear() === year;
+  // });
+}
+
+export function getMonthlyOnchainTransactionsTally(array: any[], pointsPer: number, year: number, month: number) {
+  return getMonthlyOnchainTransactions(array, year, month).length * pointsPer;
+
+  // return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+  //   const date = new Date(element.timeStamp * 1000);
+  //   return date.getFullYear() === year && date.getMonth() + 1 === month;
+  // });
 }
 
 export function getDailyOnchainTransactionsTally(
@@ -25,12 +54,14 @@ export function getDailyOnchainTransactionsTally(
   month: number,
   day: number,
 ) {
-  return getTallyForFilteredArray(array, pointsPer, (element: any) => {
-    const date = new Date(element.timeStamp * 1000);
+  return getDailyOnchainTransactions(array, year, month, day).length * pointsPer;
 
-    const isWithinYear = date.getFullYear() === year;
-    const isWithinMonth = date.getMonth() + 1 === month;
-    const isWithinDay = date.getDate() === day;
-    return isWithinYear && isWithinMonth && isWithinDay;
-  });
+  // return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+  //   const date = new Date(element.timeStamp * 1000);
+
+  //   const isWithinYear = date.getFullYear() === year;
+  //   const isWithinMonth = date.getMonth() + 1 === month;
+  //   const isWithinDay = date.getDate() === day;
+  //   return isWithinYear && isWithinMonth && isWithinDay;
+  // });
 }
