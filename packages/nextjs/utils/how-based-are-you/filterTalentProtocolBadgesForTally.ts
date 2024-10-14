a383878e107836d8f504e64d12fbc39486d2ea6f
Jacob Homanics <homanicsjake@gmail.com>
2024-10-13 20:04:39 -0500

    latest

--- packages/nextjs/utils/how-based-are-you/filterTalentProtocolBadgesForTally.ts
+++ packages/nextjs/utils/how-based-are-you/filterTalentProtocolBadgesForTally.ts
@@ -1,34 +1,34 @@
-import { getTallyForFilteredArray } from "./getTallyForFilteredArray";
+// import { getTallyForFilteredArray } from "./getTallyForFilteredArray";
 
-export function getAllTimeTalentProtocolBadgesTally(array: any[], pointsPer: number) {
-  return getTallyForFilteredArray(array, pointsPer, (element: any) => {
-    return element["onchain_at"] !== null;
-  });
-}
+// export function getAllTimeTalentProtocolBadgesTally(array: any[], pointsPer: number) {
+//   return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+//     return element["onchain_at"] !== null;
+//   });
+// }
 
-export function getYearlyTalentProtocolBadgesTally(array: any[], pointsPer: number, year: number) {
-  return getTallyForFilteredArray(array, pointsPer, (element: any) => {
-    const date = new Date(element["onchain_at"]);
-    return date.getFullYear() === year;
-  });
-}
+// export function getYearlyTalentProtocolBadgesTally(array: any[], pointsPer: number, year: number) {
+//   return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+//     const date = new Date(element["onchain_at"]);
+//     return date.getFullYear() === year;
+//   });
+// }
 
-export function getMonthlyTalentProtocolBadgesTally(array: any[], pointsPer: number, year: number, month: number) {
-  return getTallyForFilteredArray(array, pointsPer, (element: any) => {
-    const date = new Date(element["onchain_at"]);
-    return date.getFullYear() === year && date.getMonth() + 1 === month;
-  });
-}
+// export function getMonthlyTalentProtocolBadgesTally(array: any[], pointsPer: number, year: number, month: number) {
+//   return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+//     const date = new Date(element["onchain_at"]);
+//     return date.getFullYear() === year && date.getMonth() + 1 === month;
+//   });
+// }
 
-export function getDailyTalentProtocolBadgesTally(
-  array: any[],
-  pointsPer: number,
-  year: number,
-  month: number,
-  day: number,
-) {
-  return getTallyForFilteredArray(array, pointsPer, (element: any) => {
-    const date = new Date(element["onchain_at"]);
-    return date.getFullYear() === year && date.getMonth() + 1 === month && date.getDate() === day;
-  });
-}
+// export function getDailyTalentProtocolBadgesTally(
+//   array: any[],
+//   pointsPer: number,
+//   year: number,
+//   month: number,
+//   day: number,
+// ) {
+//   return getTallyForFilteredArray(array, pointsPer, (element: any) => {
+//     const date = new Date(element["onchain_at"]);
+//     return date.getFullYear() === year && date.getMonth() + 1 === month && date.getDate() === day;
+//   });
+// }
