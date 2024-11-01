59eaaae5bb3e500d7f357d9bf6ed0530cd10059c
Jacob Homanics <homanicsjake@gmail.com>
2024-10-31 19:34:37 -0500

    latest

--- packages/nextjs/components/how-based-are-you/DayCard.tsx
+++ packages/nextjs/components/how-based-are-you/DayCard.tsx
@@ -11,15 +11,15 @@ export const DayCard = ({ day, score, sources }: Props) => {
     <div className="bg-secondary rounded-lg shadow-lg w-[85px] h-[85px] md:w-[100px] md:h-[100px]">
       <div className="flex">
         <p className="text-xl m-0 p-1">{day}</p>
-        <div className="flex space-x-1 p-1">{sources}</div>
+        <p
+          className={`text-3xl md:text-3xl text-center m-0 text-primary ${
+            score === 0 ? `brightness-50` : `brightness-100`
+          }`}
+        >
+          {score}
+        </p>
       </div>
-      <p
-        className={`text-3xl md:text-3xl text-center m-0 pt-3 md:pt-8 text-primary ${
-          score === 0 ? `brightness-50` : `brightness-100`
-        }`}
-      >
-        {score}
-      </p>
+      <div className="flex flex-wrap gap-1 p-1">{sources}</div>
     </div>
   );
 };
